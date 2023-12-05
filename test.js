26d0553747a664e31d23172f8f78a9d0cd906807
Marat Tuktarov <mtuktarov@yahoo.com>
2023-12-05 04:20:04 -0800

    wip

--- test.js
+++ test.js
@@ -6,6 +6,9 @@ const octokit = new Octokit({
   auth: `${process.env.GITHUB_TOKEN}`,
   baseUrl: `https://api.github.com/repos/${process.env.INPUT_OWNER_REPO}`,
   headers: {
+
+
+    
     Accept: "application/vnd.github.v3+json",
   },
 });
